02e3c65bdef3493ef08a2ccb92416c5e37ff5009
Ghazaleh Heravi <ghazaleheravi@users.noreply.github.com>
2021-09-03 17:12:44 -0400

    fetching data is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Form from './components/Form';
 
 function App() {
+  const key = 'your_key';
+  const secret = 'your_secret';
+  //const baseURL = 'api.petfinder.com/v2';
+  let urlAuth = "https://api.petfinder.com/v2/oauth2/token";
+
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  let status = 'adoptable';
+  let breed = 'pug';
+  useEffect(() => {
+    fetch(urlAuth, {
+      method: 'POST',
+      body: `grant_type=client_credentials&client_id=${key}&client_secret=${secret}`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        return fetch('https://api.petfinder.com/v2/animals?breed=' + breed + '&status=' + status, {
+          headers: {
+            'Authorization': data.token_type + ' ' + data.access_token,
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }).then(function(res) {
+            return res.json();
+        }).then(function(json) {
+            setData(json);
+        }).catch(function(err) {
+            console.log('somthing went wrong', err.name);
+        })
+      })
+    }, []);
+  
+
   return (
     <div>
-      <form className="form">
-        <input htmlFor="name" type="text" className="searchBox"/>
-        <button className="btn">Search</button>
-      </form>
+      <Form data={data}/>
       <hr className="linebreaker" />
       <section className="section">
         <ul className="list">
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Form() {
+function Form(props) {
+  console.log(props)
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    console.log('typing');
+    e.preventDefault();
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
   return (
     <>
-      <form className="form">
-        <input htmlFor="name" type="text" className="searchBox"/>
+      <form className="form" onSubmit={handleSubmit}>
+        <input 
+          htmlFor="name" 
+          type="text" 
+          className="searchBox"
+          value={name}
+          onChange={handleChange}
+        />
         <button className="btn">Search</button>
       </form>
     </>
