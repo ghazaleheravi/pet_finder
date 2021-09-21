389a03acac8a5fae41234a5786095202f8dbce79
Ghazaleh Heravi <ghazaleheravi@users.noreply.github.com>
2021-09-21 18:32:17 -0400

    putting index.html in the same directory as index.js

---
+++ src/Form.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+function Form({ filteredData }) {
+  
+  const [input, setInput] = useState('');    //or query
+  function handleSubmit(e) {
+    e.preventDefault();
+    setInput('');
+  }
+
+  function handleSearch(e) {
+    setInput(e.target.value);
+    filteredData(e.target.value);
+  }
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input 
+          htmlFor="input" 
+          type="text" 
+          className="searchBox"
+          value={input}
+          onChange={handleSearch}
+          placeholder='Searching Animal or Breed'
+        />
+        <button className="btn">Search</button>
+      </form>
+    </>
+  );
+} 
+
+Form.propTypes = {
+  filteredData: PropTypes.function
+};
+
+export default Form;
